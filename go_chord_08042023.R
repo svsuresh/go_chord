856cf639af17b1218100c43f6fec87371e7cd5ae
suresh <kumars.sv@gmail.com>
2023-07-30 00:45:57 +0530

    made it command line

--- go_chord_08042023.R
+++ go_chord_08042023.R
@@ -1,24 +1,22 @@
 ## Load libraries
-suppressPackageStartupMessages(library("argparse"))
 suppressPackageStartupMessages(library(GOplot))
 suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(tidyr))
 suppressPackageStartupMessages(library(stringr))
 suppressPackageStartupMessages(library(janitor))
 suppressPackageStartupMessages(library(clusterProfiler))
-suppressPackageStartupMessages(library(org.Mm.eg.db))
 #######################
 ## Updated code#########
 ## ####################
+options <- commandArgs(trailingOnly = TRUE)
 
-df = read.csv("example_input.tsv",
+df = read.csv(options[1],
     sep = "\t",
     header = T
 )
-names(df)=c("symbol","fc","p_val")
 
-df <- df[abs(df$fc) > 0.6 & df$p_val < 0.05, ]
-
+###All functions########
+## Clean the data frame for empty gene symbols, identical gene symbols with different p/fc values
 clean_df <- function(x) {
     x %>%
         filter(symbol != "") %>%
@@ -30,10 +28,11 @@ clean_df <- function(x) {
         as.data.frame()
 }
 
-enrich_df <- function(x){
+## Function of enriching GO terms for all ontologies and format the ouput from enrichment to a data frame
+enrich_GO <- function(x){
     enrichGO(
         x$symbol,
-        "org.Mm.eg.db",
+        Org,
         keyType = "SYMBOL",
         ont = "ALL",
         pvalueCutoff = 0.05,
@@ -41,7 +40,7 @@ enrich_df <- function(x){
         )@result %>%
     filter(ONTOLOGY == "BP") %>%
     arrange(p.adjust) %>%
-    slice_head(n = 10) %>%
+    slice_head(n = as.integer(options[3])) %>%
     dplyr::select(1:3,7,9) %>%
     ungroup() %>%
     mutate(across(geneID, ~str_replace_all(.x, "/", ','))) %>%
@@ -56,21 +55,18 @@ enrich_df <- function(x){
     as.data.frame()
 }
 
+## Merge dataframes: GO enrichment data and cleaned data
 merge_df <- function(x,y){
     x %>%
         separate_longer_delim(Genes, ",") %>%
         inner_join(y, by = c("Genes" = "symbol")) %>%
         group_by(ID, Category) %>%
-        slice_max(order_by = abs(fc), n = 10) %>%
+        slice_max(order_by = abs(fc), n = as.integer(options[4])) %>%
         ungroup() %>%
         as.data.frame()
 }
 
-df2 <- clean_df(df)
-enrich_df2 <- enrich_df(df2)
-merged_df <- merge_df(enrich_df2,df2)
-
-## Merge two data frames two to extract final expression data frame. This data frame contains 10 statistically significant GO BP and 10 most affected (by logFC) genes. Extract genes, logFC and p-value. Change first two columns and remove duplicate rows. Convert gene symbols to upper case. This is very important. Upper case is hard coded in the code
+## Final data format for drawing the chord
 final_df <- function(x){
     x %>%
         dplyr::select(Genes,fc,p_val) %>%
@@ -80,6 +76,24 @@ final_df <- function(x){
         as.data.frame()
 }
 
+#### Build the organism of interest#####
+# Select the organism to load db
+Org_db <- switch(options[2], "mouse" = "Mm", "rat" = "Rn", "human" = "Hs")
+
+# Build the package name
+Org <- paste0("org.",Org_db,".eg.db")
+
+#Load the package name
+suppressPackageStartupMessages(require(Org, character.only = T))
+
+### all the work ####
+## Final data frame contains 10 statistically significant GO BP and 10 most affected (by logFC) genes. Extract genes, logFC and p-value. Change first two columns and remove duplicate rows. Convert gene symbols to upper case. This is very important. Upper case is hard coded in the code
+
+names(df)=c("symbol","fc","p_val")
+df <- df[abs(df$fc) > 0.6 & df$p_val < 0.05, ]
+df2 <- clean_df(df)
+enrich_df2 <- enrich_GO(df2)
+merged_df <- merge_df(enrich_df2,df2)
 gene_df <- final_df(merged_df)
 
 ## draw diagram.
